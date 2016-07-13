e1cfd3a5c25fd9245aef9c9db6a04a601a651ab0
fzhouse <fzhouse1985@gmail.com>
2016-07-13 13:52:31 +0800

    manual input and upload every 300s

--- test.py
+++ test.py
@@ -7,19 +7,33 @@ import os
 import platform
 import httplib
 import json
+import logging
 
 target = '8.8.8.8'
 processor = '10.0.63.200'
 processor_port = 8001
 
+ping_duration = 10
+ping_seg = 300
+
+logger = logging.getLogger('netdiag')
+logger.setLevel(logging.DEBUG)
+hdr = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)-15s] %(filename)s %(levelname)-4s %(message)s')
+hdr.setFormatter(formatter)
+logger.addHandler(hdr)
+
 def find_ip_loc():
-    cli = httplib.HTTPConnection('ipinfo.io', 80, timeout=30)
-    cli.request('GET', 'http://ipinfo.io')
-    res = cli.getresponse()
-    data = res.read()
-    print data
-    ipinfo = json.loads(data)
-    return ipinfo
+    try:
+        cli = httplib.HTTPConnection('ipinfo.io', 80, timeout=30)
+        cli.request('GET', 'http://ipinfo.io')
+        res = cli.getresponse()
+        data = res.read()
+        logger.info(data)
+        ipinfo = json.loads(data)
+        return ipinfo
+    except Exception, e:
+        return None
 
 def chcp():
     r = os.popen('chcp')
@@ -32,19 +46,30 @@ def chcp():
         lang = 'Chinese'
     else:
         lang = 'Others'
-    print 'Your system language is ' + lang
+    logger.info('Your system language is ' + lang)
     return code
 
 def writebase(logfile):
     fi = open(logfile, 'w+')
     ipinfo = find_ip_loc()
     baseinfo = dict()
-    baseinfo['target'] = target
-    baseinfo['ip'] = ipinfo['ip']
-    baseinfo['location'] = ipinfo['city'] + ' ' + ipinfo['region'] + ' ' + ipinfo['country']
-    baseinfo['org'] = ipinfo['org']
-    baseinfo['platform'] = platform.platform()
-    baseinfo['time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    try:
+        baseinfo['target'] = target
+        baseinfo['platform'] = platform.platform()
+        baseinfo['time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+        baseinfo['ip'] = ipinfo['ip']
+        loc = raw_input('Input your location:')
+        if loc == '':
+            baseinfo['location'] = ipinfo['city'] + ' ' + ipinfo['region'] + ' ' + ipinfo['country']
+        else:
+            baseinfo['location'] = loc
+        org = raw_input('Input your ISP:')
+        if org == '':
+            baseinfo['org'] = ipinfo['org']
+        else:
+            baseinfo['org'] = org
+    except Exception,e:
+        logger.error(e)
     jsonstr = json.dumps(baseinfo)
     fi.write(jsonstr)
     fi.close()
@@ -84,16 +109,16 @@ def traceroute(code, logfile):
             if addr.startswith(miss_str):
                 addr = '0.0.0.0'
             data += ',' + addr
-            print out
+            logger.debug(out)
             fi.write(data + '\n')
             fi.flush()
     fi.close()
 
-def ping(code, logfile):
-    cmd = 'ping -t %s' % target
-    fi = open(logfile, 'w+')
+def ping(code, count, offset, logfile):
+    cmd = 'ping -n %d %s' % (count, target)
+    fi = open(logfile, 'a')
     p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-    seq = 1
+    seq = offset+1
     while True:
         out = p.stdout.readline()
         if out == '':
@@ -107,7 +132,7 @@ def ping(code, logfile):
                 hit_str = '来自'
                 miss_str = '请求超时'
             if out.startswith(hit_str):
-                print out
+                logger.debug(out)
                 data = '%d,' % seq
                 outs = out.split()
                 byts = outs[3].split('=', 1)[1]
@@ -116,7 +141,7 @@ def ping(code, logfile):
                 data += '%s,%s,%s' % (byts, delay, ttl)
                 seq += 1
             elif out.startswith(miss_str):
-                print out
+                logger.debug(out)
                 data = '%d,' % seq
                 data += '0,-1,0'
                 seq += 1
@@ -127,21 +152,43 @@ def ping(code, logfile):
     fi.close()
 
 def upload(logfile):
+    logger.info("uploading %s" % logfile)
     fi = open(logfile, 'rb')
     data = fi.read()
     fi.close()
-    headers = {"Content-type": "text/plain"}
-    srv_url = "http://%s:%d/file/%s" % (processor, processor_port, logfile)
-    cli = httplib.HTTPConnection(processor, processor_port, timeout=30)
-    cli.request("PUT", srv_url, data, headers)
-    res = cli.getresponse()
-    if res.status == 200:
-        return 0
-    return 1
+    try:
+        headers = {"Content-type": "text/plain"}
+        srv_url = "http://%s:%d/file/%s" % (processor, processor_port, logfile)
+        cli = httplib.HTTPConnection(processor, processor_port, timeout=10)
+        cli.request("PUT", srv_url, data, headers)
+        res = cli.getresponse()
+        if res.status == 200:
+            return 0
+        return 1
+    except Exception, e:
+        logger.error(e)
+        return 1
+
+def upload_and_delete(logfile, retry=3):
+    ret = 1
+    count = 0
+    while ret != 0:
+        count += 1
+        if count == retry+1:
+            break
+        ret = upload(logfile)
+        if ret == 0:
+            os.system('del %s' % logfile)
 
 if __name__ == '__main__':
-    print 'PALLN test starting...'
-    print 'Target is %s' % target
+    logger.info('PALLN test starting...')
+    logger.info('Target is %s' % target)
+
+    try:
+        ping_duration = int(raw_input("Ping duration(minutes):"))
+        ping_count = ping_duration * 60
+    except Exception,e:
+        logger.error(e)
 
     code = chcp()
 
@@ -149,36 +196,21 @@ if __name__ == '__main__':
 
     baselog = 'base_%s.log' % tid
     writebase(baselog)
-    ret = 1
-    count = 0
-    while ret != 0:
-        count += 1
-        if count == 4:
-            break
-        ret = upload(baselog)
-        if ret == 0:
-            os.system('del %s' % baselog)
+    upload_and_delete(baselog)
 
     trlog = 'tracert_%s.log' % tid
     traceroute(code, trlog)
-    ret = 1
-    count = 0
-    while ret != 0:
-        count += 1
-        if count == 4:
-            break
-        ret = upload(trlog)
-        if ret == 0:
-            os.system('del %s' % trlog)
+    upload_and_delete(trlog)
 
     pinglog = 'ping_%s.log' % tid
-    ping(code, pinglog)
-    ret = 1
-    count = 0
-    while ret != 0:
-        count += 1
-        if count == 4:
-            break
-        ret = upload(pinglog)
-        if ret == 0:
-            os.system('del %s' % pinglog)
+    offset = 0
+    while ping_count > 0:
+        if ping_count > ping_seg:
+            count = ping_seg
+        else:
+            count = ping_count
+        ping(code, count, offset, pinglog)
+        ping_count = ping_count - count
+        offset = offset + count
+        logger.info('finished %d' % offset)
+        upload_and_delete(pinglog)
